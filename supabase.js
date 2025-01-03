4710d272d1c0f1247d0172ff3e0506339a65ccab
divmittal5 <59502624+divmittal5@users.noreply.github.com>
2025-01-04 04:19:13 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
 // supabase.js
 
-// Import the Supabase client
-const { createClient } = supabase;
+
 
 // Initialize Supabase connection
 const SUPABASE_URL = 'https://rcjpzwpoyxgqyazourla.supabase.co';  // Replace with your Supabase URL
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjanB6d3BveXhncXlhem91cmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5MzgyNzYsImV4cCI6MjA1MTUxNDI3Nn0.bl0y1oyf9fiyhGL2xyNNpsthrqMax1QFbK0nmotF9uo';  // Replace with your Supabase API key
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
